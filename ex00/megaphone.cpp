5a25ed71ec5383ea917d0daa8fc858fb4531d125
Valentin Fuhlenbrok <minh.tee@gmail.com>
2023-02-19 14:04:41 +0100

    .

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/29 22:28:16 by vfuhlenb          #+#    #+#             */
+/*   Updated: 2023/02/05 19:14:47 by vfuhlenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	word = 0;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (++word < argc)
+	{
+		for (size_t letter = 0; argv[word][letter]; letter++)
+			std::cout << (char)toupper(argv[word][letter]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
